110e4b3640eb6cff4264948e1fdb3fe115e331f6
bisketashwin <bisketashwin@gmail.com>
2023-08-29 13:09:31 +0530

    Drawer implemented and working fine, modal transition is not fixed yet

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import LogIn from './components/LogIn';
 import TabsLayout from './components/Tabs/TabsLayout';
 import AddFlowStack from './components/AddFlow/AddFlowStack';
@@ -10,13 +12,25 @@ import MyModal from './components/MyModal';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator options={{headerShown:false}}>
+      {/* Bellow line of code would hide the redundant tabs menu item and also hode the tabs title bar which are shown by line wihtout this comment */}
+      {/*<Drawer.Screen name="Feed" component={TabsLayout}  options={{drawerItemStyle: { display: 'none' },headerShown:false }}/> */}
+      <Drawer.Screen name="Feed" component={TabsLayout} />
+    </Drawer.Navigator>
+  );
+}
+
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="LogIn" component={LogIn} />
-        <Stack.Screen name="TabsLayout" component={TabsLayout} options={{headerShown:false}}/>
+        <Stack.Screen name="TabsLayout" component={MyDrawer} options={{headerShown:false}}/>
         <Stack.Screen name="AddFlowStack" component={AddFlowStack} options={{headerShown:false}} />
         <Stack.Screen name="MyModal" component={MyModal} options={{presentation:'card'}}/>
       </Stack.Navigator>
--- components/Tabs/Tab2.jsx
+++ components/Tabs/Tab2.jsx
@@ -1,19 +1,29 @@
-import { Text, View , Button,TouchableOpacity} from 'react-native'
+import { Text, View, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { buttonStyles, containersStyles } from '../styles/CommonStyles'
 
+
+
 const Tab2 = () => {
   const Navigator = useNavigation()
 
-  const HandleLogIn = ()=>{
-      Navigator.navigate('AddFlowStack')
-    }
+  const HandleLogIn = () => {
+    Navigator.navigate('AddFlowStack')
+  }
+  const HandleShowMenu = () => {
+    Navigator.navigate('AddFlowStack')
+  }
   return (
     <View style={containersStyles.con1}>
       <Text>Tab2</Text>
       <TouchableOpacity title='Add Items' onPress={HandleLogIn} style={buttonStyles.button1}>
-<Text style={buttonStyles.button1Text}>Add Items</Text>
+
+        <Text style={buttonStyles.button1Text}>Add Items</Text>
+      </TouchableOpacity>
+      <TouchableOpacity title='Show Menu' onPress={HandleShowMenu} style={buttonStyles.button1}>
+
+        <Text style={buttonStyles.button1Text}>Show Menu</Text>
       </TouchableOpacity>
     </View>
   )
