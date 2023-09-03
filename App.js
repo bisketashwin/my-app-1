6f65e48fcd4f389e620f5870494ad981b51cfd0d
bisketashwin <bisketashwin@gmail.com>
2023-09-03 19:02:26 +0530

    Added Safe Area view
    and some clean up

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import * as React from 'react';
-import { Button, View, Text, Alert } from 'react-native';
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from '@rneui/base';
 // this will hide alert messages in expo go
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs();//Ignore all log notifications
 
-import { containers, buttons, Texts } from './components/Styles';
 
-import AddWithdraw from './components/TabScreens/AddWithdraw';
-import Receitps from './components/TabScreens/Receitps';
-import Trading from './components/TabScreens/Trading';
-import Lending from './components/TabScreens/Lending';
-import PNL from './components/TabScreens/PNL';
+
 import { MyDrawer } from './components/Draw/MyDrawer';
 import LogIn from './components/LogIn';
 import Feed from './components/Feed';
@@ -26,51 +19,18 @@ export function EmptyScreen() {
 }
 
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-// export const TabsLayout = () => {
-//     return (
-//         <Tab.Navigator name='Tabs Parent'>
-//             <Tab.Screen name="Tab1" component={AddWithdraw}
-//                 options={{
-//                     title: 'Add Withdraw', headerShown: false,
-//                     tabBarIcon: ({ color, size }) => (<Icon name="shipping-fast" type='font-awesome-5' color={color} size={size}
-//                     />)
-//                 }} />
-//             <Tab.Screen name="Tab2" component={Receitps}
-//                 options={{
-//                     title: 'Receipts', headerShown: false,
-//                     tabBarIcon: ({ color, size }) => (<Icon name="archive" type='entypo' color={color} size={size} />)
-//                 }} />
-//             <Tab.Screen name="Tab3" component={Trading}
-//                 options={{
-//                     title: 'Trading', headerShown: false,
-//                     tabBarIcon: ({ color, size }) => (<Icon name="clipboard-list" type='font-awesome-5' color={color} size={size} />)
-//                 }} />
-//             <Tab.Screen name="Tab4" component={Lending}
-//                 options={{
-//                     title: 'Lending', headerShown: false,
-//                     tabBarIcon: ({ color, size }) => (<Icon name="hand-coin" type='material-community' color={color} size={size} />)
-//                 }} />
-//             <Tab.Screen name="Tab5" component={PNL}
-//                 options={{
-//                     title: 'PNL', headerShown: false,
-//                     tabBarIcon: ({ color, size }) => (<Icon name="chart-line" type='font-awesome-5' color={color} size={size} />)
-//                 }} />
-//         </Tab.Navigator>
-//     )
-// }
-
 
 function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Log In" component={LogIn} options={{ headerShown: false }} />
-                <Stack.Screen name="MyDrawer" component={MyDrawer} options={{ headerShown: false }} />
-                <Stack.Screen name="Feed" component={Feed} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Log In" component={LogIn} options={{ headerShown: false }} />
+                    <Stack.Screen name="MyDrawer" component={MyDrawer} options={{ headerShown: false }} />
+                    <Stack.Screen name="Feed" component={Feed} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
     );
 }
 
